17040b1e552e423b6475ad193b292c318c8d5e6c
Amanuel Tesfamichael <amantesfazgi19@gmail.com>
2020-02-13 01:11:28 -0500

    Added details view, tab navigation and collection view

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Amanuel Tesfazgi Tesfamichael on 2020/02/13.
+//  Copyright © 2020 Amanuel Tesfazgi Tesfamichael. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
